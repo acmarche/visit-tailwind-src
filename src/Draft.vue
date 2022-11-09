a3b99ee13d7dd013fdfaa345d06f5bde6f53db01
jfsenechal <jfsenechal@gmail.com>
2022-11-09 19:44:18 +0100

    add discover

--- src/Draft.vue
+++ src/Draft.vue
@@ -1,47 +1,6 @@
 <script setup>
-import Header from "@/components/header/Header.vue";
-import NavIconHomeMobile from '@/components/header/mobile/NavIconHomeMobile.vue'
-import NavIconMenuMobileSvg from '@/components/header/mobile/NavIconMenuMobileSvg.vue'
-import NavIconCommuneShortCutMobile from "@/components/header/mobile/NavIconCommuneShortCutMobile.vue";
-import NavItemsMobile from "@/components/header/mobile/NavItemsMobile.vue";
-import NavIconSearchMobile from "@/components/header/mobile/NavIconSearchMobile.vue";
-import Navigation from "@/components/header/Navigation.vue";
-
-function openMenuVivre() {
-  const menu = document.querySelector("#menu-organise");
-  menu.style.top = "0";
-}
+import Decouvrir from "@/components/article/Decouvrir.vue";
 </script>
-
 <template>
-  <Header :image="''"/>
-  <Navigation/>
-
+  <Decouvrir/>
 </template>
-<style>
-
-li.nav-top {
-  @apply xl:p-3 xl:text-cta-dark
-  xl:transition-shadow xl:duration-300 xl:ease-in-out;
-}
-
-li.nav-top:hover {
-  box-shadow: 0 -3px 0 0 #4ae0c7 inset;
-}
-
-.btn-vivre:hover > nav {
-  @apply xl:block
-}
-
-.icon-menu-mobile {
-  @apply my-4 flex flex-col flex-wrap items-center justify-center font-montserrat-semi-bold;
-}
-
-.text-menu-mobile {
-  @apply text-xs font-montserrat-light;
-}
-
-.musshadow {
-  box-shadow: 0 -0.125rem 0.45rem rgba(0, 0, 0, 0.75);
-}
-</style>
---
+++ src/components/article/Decouvrir.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="decouvrir-page flex flex-col flex-wrap justify-center pt-4 mx-auto w-2/3 ">
+    <li class="w-40text-center pt-3">
+      <a href="https://visitmarche.be/fr/categorie/patrimoine/" class="flex items-center justify-start ml-auto   cat-pat">
+        <figure>
+          <img src="images/eglise-decouvrir.png" class="w-24" alt="">
+        </figure>
+        <span class="">Patrimoine</span>
+      </a>
+    </li>
+    <li class="w-40text-center pt-3">
+      <a href="https://visitmarche.be/fr/categorie/arts/" class="flex items-center justify-start ml-auto   cat-pat">
+        <figure>
+          <img src="images/statue-decouvrir.png" class="w-24" alt="">
+        </figure>
+        <span class="">Arts</span>
+      </a>
+    </li>
+    <li class="w-40text-center pt-3">
+      <a href="https://visitmarche.be/fr/categorie/balades/" class="flex items-center justify-start ml-auto   cat-pat">
+        <figure>
+          <img src="images/sac-dos-decouvrir.png" class="w-24" alt="">
+        </figure>
+        <span class="">Balades</span>
+      </a>
+    </li>
+    <li class="w-40text-center pt-3">
+      <a href="https://visitmarche.be/fr/categorie/gourmandises/" class="flex items-center justify-start ml-auto   cat-pat">
+        <figure>
+          <img src="images/chou-decouvrir.png" class="w-24" alt="">
+        </figure>
+        <span class="">Gourmandises</span>
+      </a>
+    </li>
+    <li class="w-40text-center pt-3">
+      <a href="https://visitmarche.be/fr/categorie/fetes/" class="flex items-center justify-start ml-auto   cat-pat">
+        <figure>
+          <img src="images/tambour-decouvrir.png" class="w-24" alt="">
+        </figure>
+        <span class="">Fêtes</span>
+      </a>
+    </li>
+  </ul>
+</template>
