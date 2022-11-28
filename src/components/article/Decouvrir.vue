4921354868b127b4d5c591a63bce3dcd68db09d6
jfsenechal <jfsenechal@gmail.com>
2022-11-28 09:47:13 +0100

    better

--- src/components/article/Decouvrir.vue
+++ src/components/article/Decouvrir.vue
@@ -1,20 +1,27 @@
 <script setup>
 const icones = [
-  {id: 1, nom: 'Patrimoine', image: 'images/eglise-decouvrir.png', url: '/'},
-  {id: 2, nom: 'Arts', image: 'images/statue-decouvrir.png', url: '/'},
-  {id: 3, nom: 'Balades', image: 'images/sac-dos-decouvrir.png', url: '/'},
-  {id: 4, nom: 'Gourmandises', image: 'images/chou-decouvrir.png', url: '/'},
-  {id: 5, nom: 'Fêtes', image: 'images/tambour-decouvrir.png', url: '/'},
+  {id: 1, nom: 'Patrimoine', image: 'images/eglise-decouvrir.png', url: '/fr/?cat=9'},
+  {id: 2, nom: 'Arts', image: 'images/statue-decouvrir.png', url: '/fr/?cat=10'},
+  {id: 3, nom: 'Balades', image: 'images/sac-dos-decouvrir.png', url: '/fr/?cat=11'},
+  {id: 4, nom: 'Gourmandises', image: 'images/chou-decouvrir.png', url: '/fr/?cat=13'},
+  {id: 5, nom: 'Fêtes', image: 'images/tambour-decouvrir.png', url: '/fr/?cat=12'},
 ]
+// /wp-content/themes/visittail/assets/tartine/
 </script>
 <template>
-  <ul class="decouvrir-page flex flex-col flex-wrap justify-center pt-4 mx-auto w-2/3">
+  <ul class="list-none decouvrir-page flex flex-col flex-wrap justify-center pt-4 mx-auto w-2/3">
     <li class="group w-40text-center pt-3" v-for="item in icones" :key="item.id">
       <a :href="item.url" class="flex items-center justify-start ml-auto  rounded-lg shadow-lg cat-pat">
         <figure>
-          <img :src="item.image" class="w-36" alt="">
+          <img :src="item.image" class="w-32" alt="">
         </figure>
         <span class="text-cta-dark text-lg group-hover:text-cta-light">{{ item.nom }}</span>
+        <span class="mr-12 ml-auto group-hover:text-cta-light">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+               stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
+          </svg>
+        </span>
       </a>
     </li>
   </ul>
